44ce4221b1c986ac679276ec788511df16876bba
Luis Rivera <luisriveradiaz1699@gmail.com>
2024-11-07 13:43:53 -0500

    xddasda

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/upload", async (req, res) => {
     const b = await put(req.query.filename, req, {
         access: 'public',
     });
-    res.json({ location: `${b}` });
+    res.json({ location: `${b.url}` });
 });
 
 app.listen(PORT, () => {
